79e62f3573bd763063d2a6793cafeb2a5bf3f8e7
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-02-17 14:16:17 +0200

    12

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,38 @@ const App = () => {
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // is item already in cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
 
-  const handleRemoveFromCart = () => null;
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // first time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(
+      (prev) => (
+        prev.reduce((ack, item) => {
+          if (item.id === id) {
+            if (item.amount === 1) return ack;
+            return [...ack, { ...item, amount: item.amount - 1 }];
+          } else {
+            return [...ack, item]
+          }
+        },  [] as CartItemType[])
+       
+      )
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>;
